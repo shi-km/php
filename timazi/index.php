5aeafa0eaa9241ec5d890032cb06f4aab36725ca
shi-km <julietwanjiru333@gmail.com>
2024-10-14 21:17:49 +0300

    more php code

--- timazi/index.php
+++ timazi/index.php
@@ -6,16 +6,63 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="index.php" method="post">
+      <input type="radio" name="credit_card" value="visa">
+      visa<br>
+      <input type="radio" name="credit_card" value="Mastercard">
+      Mastercard<br>
+      <input type="radio" name="credit_card" value="American Express">
+      American Express<br>
+      <input type="submit" name="confirm" value="confirm"><br>
+    </form>
     <?php
 
-    $capitals=array("Nairobi","kisumu","meru","nakuru","eldoret");
-    array_pop($capitals);
-    array_push($capitals,"Embu");
+$credit_card=null;
+      if(isset($_POST["confirm"])){
+        $credit_card = $_POST["credit_card"];
+      }
+      switch($credit_card){
+        case "visa";
+        echo "you have selected visa";
+        break;
+        case "Mastercard";
+        echo "you have selected Mastercard";
+        break;
+        case "American Express";
+        echo "you have selected American Express";
+        break;
+        default: echo "you have not made a selection";
+      }
+
+    //   if(isset($_POST["login"])){
+    //      $username= $_POST["username"];
+    //      $password=$_POST["password"];
+
+    //      if(empty($username)){
+    //         echo "Please Enter  username";
+    //      }
+
+    //      elseif(empty($password)) {
+    //      echo "please enter password .<br>";
+          
+    //      }
+
+    //      else{
+    //         echo "welcome {$username} Your log in info is . <br>";
+    //      }
+    //   }
+    //   foreach($_POST as $key => $value){
+    //     echo "{$key} = {$value}. <br>";
+    //   }
+
+    // $capitals=array("Nairobi","kisumu","meru","nakuru","eldoret");
+    // array_pop($capitals);
+    // array_push($capitals,"Embu");
    
 
-    foreach($capitals as $capital){
-        echo $capital . "<br>";
-    }
+    // foreach($capitals as $capital){
+    //     echo $capital . "<br>";
+    // }
     //foraech function is used to loop between arrays
     //arrays are variables that store several values at once 
 
@@ -23,21 +70,37 @@
     //shift function removes the firt element in an array
  
 
-    $cars=array("subaru"=>"forestar","nissan"=>"juke","Mercedes"=>"c200","prado"=>"v8");
-    array_pop($cars);
-    $cars["prado"]="crusa";
+    // $cars=array("subaru"=>"forestar","nissan"=>"juke","Mercedes"=>"c200","prado"=>"v8");
+    // array_pop($cars);
+    // $cars["prado"]="crusa";              
 
 
-    foreach($cars as $key => $value){
-        echo "{$key} = {$value}"  ."<br>";
-    }
+    // foreach($cars as $key => $value){
+    //     echo "{$key} = {$value}"  ."<br>";
+    // }
  
     //For loop
-    $count=null;
-    for($i=1; $i <=6;$i++){
-     echo $i ."<br>";
-    }
+    //for loop has three contions 
+
+    // $count=$_POST["counter"];
+    // for($i=1; $i <=$count;$i++){
+    //  echo $i ."<br>";
+    // }
+
+    //two important php functions 
+    //isset() and empty()
+    // isset function returns true if a variable is declared and not null.
+
+    // $var=30;
+
+    // if(isset($var)){
+    //     echo "This is a set";
+    // }
+    // else{
+    //     echo "This is not a set";
+    // }
     
+
     ?>
 </body>
 </html>
